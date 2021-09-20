c5a7068d4497519be44bc57c70ede36faa53d084
sabiyoung <ethiodatecakes@gmail.com>
2021-09-20 16:53:47 -0700

    created express-app brunch

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,10 +1,12 @@
 import express from 'express';
 const app = express();
-const port = 3002
+const port = 3000;
+
 
 app.get('/', function(req, res) {
-    res.json({test: 'Hello'})
+    res.json({test: 'express-app'})
   });
+
 
 app.listen(port, function() {
     console.log(`running on http://localhost:${port}`)
