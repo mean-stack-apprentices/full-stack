f812da6eb0addd14829f994cf440d4e87338ef17
mekdes45 <mekmis2020@gmail.com>
2021-09-21 08:40:07 -0700

    Added Express-app-User.json

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,6 +2,16 @@ import express from "express";
 
 const port = 3000;
 const app = express();
+import cors from "cors";
+import path from "path";
+const __dirname = path.resolve();
+
+
+app.use(cors());
+app.get('/users', function (req, res) {
+  res.sendFile(path.join(__dirname, 'users.json'));
+   
+});
 
 app.get('/', function (req, res) {
   console.log('app')
