ac6f3c932636ed3e7c0dfa6aa93a2555810d159b
sabiyoung <ethiodatecakes@gmail.com>
2021-09-21 08:40:34 -0700

    feat(express): added express structure

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,10 +3,12 @@ const app = express();
 const port = 3000;
 
 
+
 app.get('/', function(req, res) {
     res.json({test: 'express-app'})
   });
 
+  
 
 app.listen(port, function() {
     console.log(`running on http://localhost:${port}`)
