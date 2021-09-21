5b85837e1d7c17510482b1106e37297630c49105
ChelleAmon <amongichelle@gmail.com>
2021-09-21 12:12:35 -0700

    feat(api): create get function

--- src/client/src/app/services/api.service.ts
+++ src/client/src/app/services/api.service.ts
@@ -6,5 +6,11 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
 
+  baseUrl = 'http://localhost:3501/';
+
   constructor(private http: HttpClient) { }
+
+  get<T>(resource:string){
+    return this.http.get<T>(this.baseUrl + resource);
+  }
 }
