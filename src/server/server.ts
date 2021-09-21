9f64d5b9658c23ce044d53de3c7dbc7d59ef218e
mekdes45 <mekmis2020@gmail.com>
2021-09-21 11:10:29 -0700

    added

---
+++ src/server/server.ts
@@ -0,0 +1,15 @@
+import express from 'express';
+
+const port = 3000;
+
+const app = express();
+app.get('/', function(req, res) {
+  res.json({test: 'test'})
+});
+
+
+
+
+app.listen(port, function () {
+    console.log(`running on http://localhost:${port}`)
+  })
