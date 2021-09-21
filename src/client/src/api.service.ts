88d03a1de8b2ad3fa3aa7d67449c5a8243802594
sirpuffin1 <angelcruz981@yahoo.com>
2021-09-21 11:32:44 -0700

    configure api service

--- src/client/src/api.service.ts
+++ src/client/src/api.service.ts
@@ -1,9 +1,15 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http'
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
-
-  constructor() { }
+  root="http://localhost:3501"
+  constructor(private http: HttpClient) {
+    console.log(this.get('users'))
+   }
+   get<T>(path: string) {
+     return this.http.get<T>(this.root + path)
+   }
 }
