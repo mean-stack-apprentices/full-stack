b7684d4b5c84da9b6f476f51c55e13dcc4bcf181
sirisha v <svoggu@alphaworks.tech@gmail.com>
2021-09-22 15:22:37 -0700

    refactor(server,route):update

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,10 +2,19 @@ import express from 'express';
 import cors from 'cors';
 import fs from 'fs';
 import path from 'path';
+import mongoose from 'mongoose';
+import { UserModel } from './schemas/user.schema';
  
 const app = express();
 const __dirname = path.resolve();
 const PORT = 3501;
+
+mongoose.connect('mongodb://localhost:27017/test')
+.then(() => {
+    console.log('Connected to DB Successfully');
+})
+.catch(err => console.log(err, 'connecting to db'));
+
 
 app.use(cors());
 
@@ -15,7 +24,12 @@ app.get('/', function(req, res) {
 });
 
 app.get('/users', function(req,res){
-    res.sendFile(path.join(__dirname, 'users.json'));
+    UserModel.find()
+    .then(users => res.json({data: users}))
+    .catch(err => {
+        res.status(501)
+        res.json({errors: err})
+    })
 });
 
 
