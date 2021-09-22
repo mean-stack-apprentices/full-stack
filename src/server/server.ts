58d799acecddec67594df1867a2dabc84fa7d9b9
mekdes45 <mekmis2020@gmail.com>
2021-09-21 20:40:15 -0700

    feat(server,mongoose): create user schema interface and model.

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,24 +1,54 @@
 import express from 'express';
 import cors from 'cors';
-import fs from 'fs';
 import path from 'path';
- 
-const app = express();
+import mongoose from 'mongoose';
+import { UserModel } from './schemas/user.schema.js';
+
+mongoose .connect('mongodb://localhost:27017/test')
+  .then(() => {
+    console.log('connected to db');
+  }).catch(err => console.log(err, 'connecting to db'));
+
+  const app = express();
 const __dirname = path.resolve();
 const PORT = 3501;
 
 app.use(cors());
-
-
 app.get('/', function(req, res) {
-   res.json({message:'test'});
-});
-
-app.get('/users', function(req,res){
-    res.sendFile(path.join(__dirname, 'users.json'));
+  res.json({message:'test'});
 });
 
 
-app.listen(PORT, function(){
-    console.log( `starting at localhost http://localhost: ${PORT}`);
+app.get('/users', function (req, res) {
+  UserModel.find().then(users => {
+    console.log('found users', users);
+    res.json({data:users})
+});
+
+});
+
+app.get('/create-user', function (req, res) {
+  const user = new UserModel({
+    name: 'Mekdes',
+    email: 'mekdesb123@bitwise.com'
+  
+  });
+
+  app.get('/create-user', function (req, res) {
+    const user = new UserModel({
+      name: 'sol',
+      email: 'solomon@bitwise.com'
+    
+    });
+
+    user.save().then(user => {
+      console.log(user, 'saved')
+      res.json({ data: user });
+    })
+  });
+
+
+  app.listen(PORT, function () {
+    console.log(`starting at localhost http://localhost: ${PORT}`);
+  })
 })
