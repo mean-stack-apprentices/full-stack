c5782ad37280822d6e70441c5e99fd3dd2c70aeb
sabiyoung <ethiodatecakes@gmail.com>
2021-09-21 20:32:45 -0700

    feat(server.ts) add user route & connect db

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,7 +2,14 @@ import express from 'express';
 import cors from 'cors';
 import fs from 'fs';
 import path from 'path';
- 
+import mongoose from 'mongoose';
+import { UserModel } from './schemas/user.schema.js';
+
+
+mongoose.connect('mongodb://localhost:27017/test').then(() => {
+      console.log('connected to db');
+  }).catch(err => console.log(err, 'connecting to db'))
+
 const app = express();
 const __dirname = path.resolve();
 const PORT = 3501;
@@ -15,9 +22,24 @@ app.get('/', function(req, res) {
 });
 
 app.get('/users', function(req,res){
-    res.sendFile(path.join(__dirname, 'users.json'));
+    UserModel.find().then(users => {
+        console.log('found users', users);
+        res.json({data: users})
+    })
 });
 
+app.get('/create-user', function(req, res) {
+    const user = new UserModel({
+      
+        name: 'Saba',
+        email: 'saba@bitwise.com'
+    });
+
+    user.save().then(user => {
+        console.log(user, 'saved');
+        res.json({data: user})
+    })
+})
 
 app.listen(PORT, function(){
     console.log( `starting at localhost http://localhost: ${PORT}`);
