4733e4b1ffa58ab145a4c023d71f17ce8ee87b60
swz22 <jkim005@yahoo.com>
2021-09-22 14:55:31 -0700

    initial commit john branch

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,7 +2,18 @@ import express from 'express';
 import cors from 'cors';
 import fs from 'fs';
 import path from 'path';
+import mongoose from 'mongoose'
  
+// mongoose.connect('mongodb://localhost:12701/testdb1')
+// .then(()=> {
+//     console.log('db connected')
+// })
+// .catch(err => console.log(err))
+
+mongoose.connect('mongodb://localhost:27017/test').then(() => {
+      console.log('successfully connected to db');
+  }).catch(err => console.log(err, 'error connecting to db'))
+98
 const app = express();
 const __dirname = path.resolve();
 const PORT = 3501;
