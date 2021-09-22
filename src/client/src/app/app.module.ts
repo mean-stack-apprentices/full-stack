ed48a11ac81c071de2dac9e52acca010be9492da
Bryce <71417376+brycejjacks@users.noreply.github.com>
2021-09-22 14:39:36 -0700

    feat(client.userInput): + form

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -1,6 +1,7 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { UsersListComponent } from './components/users-list/users-list.component';
@@ -16,6 +17,8 @@ import { UserInputComponent } from './components/user-input/user-input.component
     BrowserModule,
     AppRoutingModule,
     HttpClientModule,
+    FormsModule,
+    ReactiveFormsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/client/src/app/components/user-input/user-input.component.ts
+++ src/client/src/app/components/user-input/user-input.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
@@ -6,8 +7,15 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user-input.component.scss']
 })
 export class UserInputComponent implements OnInit {
-
-  constructor() { }
+  addUser: FormGroup;
+  constructor(
+    private fb: FormBuilder,
+  ) {
+    this.addUser = this.fb.group({
+      name: ['',Validators.required],
+      username: ['',Validators.compose([Validators.required, Validators.minLength(3),])],
+    })
+  }
 
   ngOnInit(): void {
   }
