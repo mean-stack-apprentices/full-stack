a494113cb8c8384a5afebf695ead04773658875a
mekdes45 <mekmis2020@gmail.com>
2021-09-22 08:55:23 -0700

    add user class

--- src/client/src/app/models/user.ts
+++ src/client/src/app/models/user.ts
@@ -1,7 +1,7 @@
 export class User {
     "id": 1;
     "name": "Leanne Graham";
-    "username": "Bret";
+    "username": string;
     "email": "Sincere@april.biz";
     "address": {
         "street": "Kulas Light",
