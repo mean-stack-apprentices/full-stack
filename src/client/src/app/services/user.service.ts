6815b606c870e901a3478e01b3213421083486d1
LeeTato <leetato25@gmail.com>
2021-09-22 00:33:35 -0500

    feat(userServices): created user service

---
+++ src/client/src/app/services/user.service.ts
@@ -0,0 +1,21 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { User } from '../models/user';
+import { ApiService } from './api.service';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+ users$: Observable<User[]>= new Observable((Observer) => {
+  Observer.next([]);
+});
+
+  constructor(private api:ApiService) {
+
+  }
+
+  getUsers(){
+    this.users$ = this.api.get<User[]>('users')
+  }
+}
