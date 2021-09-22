25a6f080799c6917f44ef2c5eebda230e316505a
sirisha v <svoggu@alphaworks.tech@gmail.com>
2021-09-22 00:23:40 -0700

    feat(server): Add schema for users

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,6 +4,22 @@ import fs from 'fs';
 import path from 'path';
 import mongoose from 'mongoose';
 
+const {Schema, model} = mongoose;
+interface User {
+        id: number;
+	    name: string;
+        username: string;
+	    email: string;
+	  }
+
+ // 2. Create a Schema corresponding to the document interface.
+ const userSchema = new Schema<User>({
+    id: { type: Number, required: true },
+    name: { type: String, required: true },
+    username: { type: String, required: true },
+    email: { type: String, required: true },
+    
+  });
 
 mongoose.connect('mongodb://localhost:27017/test').then(() => {
 console.log('connected to db');
