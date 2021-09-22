685ba32ca2f565c81cca24af5bda667cbb7326a4
LeeTato <leetato25@gmail.com>
2021-09-21 22:37:26 -0500

    feat(server): created user route

--- src/server/server.ts
+++ src/server/server.ts
@@ -4,16 +4,24 @@ import fs from 'fs';
 import path from 'path';
 
 import mongoose from 'mongoose';
+import { UserModel } from './schemas/user.schema';
 mongoose.connect('mongodb://localhost:27017/test').then(() => {
       console.log('connected to db');
   }).catch(err => console.log(err, 'connecting to db'))
 
-const app = express();
-const __dirname = path.resolve();
-const PORT = 3501;
 
-app.use(cors());
-
+  const app = express();
+  const __dirname = path.resolve();
+  const PORT = 3501;
+  
+  app.use(cors());
+  
+  app.get('/create-user', function (req, res) {
+    const user = new UserModel({
+      name: 'Lielt',
+      email: 'lee@bitwise.com'
+    
+    });
 
 app.get('/', function(req, res) {
    res.json({message:'test'});
