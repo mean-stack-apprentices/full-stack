028183334c1fe512771b75c35f82aabf3eee295d
Bryce <71417376+brycejjacks@users.noreply.github.com>
2021-09-22 14:10:28 -0700

    feat(shared.models): + model for client & server

--- src/server/schemas/user.schema.ts
+++ src/server/schemas/user.schema.ts
@@ -1,15 +1,11 @@
 import mongoose from 'mongoose';
+import type { User } from '../../shared/models/user.model';
 const {Schema, model} = mongoose
 
 mongoose.connect('mongodb://localhost:27017/full-stack')
 .then(() => {
     console.log('conneted to db')
 }).catch(err => console.log(err, 'error connecting to db'))
-
-interface User {
-    name: string,
-    username: string,
-}
 
 const userSchema = new Schema<User>({
     name: {type: String, required: true},
