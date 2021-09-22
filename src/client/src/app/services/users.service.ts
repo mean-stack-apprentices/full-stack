e7cb98c92f5371ab21db82c9daf2f6de6a8ae783
sirisha v <svoggu@alphaworks.tech@gmail.com>
2021-09-22 01:29:56 -0700

    feat(client): Add users service and get users

---
+++ src/client/src/app/services/users.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
+import { ApiService } from './api.service';
+import { User } from '../schemas/user';
+@Injectable({
+  providedIn: 'root'
+})
+export class UsersService {
+
+users$: Observable<User[]> = new Observable((observer) => {
+  observer.next([])
+});
+
+  constructor(private apiService: ApiService) { }
+
+getUsers() {
+  this.users$ = this.apiService.get<User[]>('users')
+}
+
+}
