fe2377d43ecf76c8c60916ecf6889a63113f280e
sabiyoung <ethiodatecakes@gmail.com>
2021-09-21 22:37:25 -0700

    feat(api.service.ts) add post method

--- src/client/src/app/services/api.service.ts
+++ src/client/src/app/services/api.service.ts
@@ -6,10 +6,13 @@ import { Injectable } from '@angular/core';
 })
 export class ApiService {
   baseUrl:string = 'http://localhost:3501/';
-  constructor(private http: HttpClient) 
+  constructor(private http: HttpClient)
   { }
 
   get<T>(resourceName: string) {
     return this.http.get<T>(this.baseUrl + resourceName);
+  }
+  post<T, D>(resourceName: string, data: D) {
+    this.post<T, D>(this.baseUrl + resourceName, data)
   }
 }
