a584bf51c184deaa7705c4a5b617b872c1734cc3
Bryce <71417376+brycejjacks@users.noreply.github.com>
2021-09-23 16:22:40 -0700

    refactor(post.schema): start to relate to users

--- src/server/schemas/post.schema.ts
+++ src/server/schemas/post.schema.ts
@@ -4,7 +4,7 @@ import type { Post } from '../../shared/models/post.model'
 const postSchema = new mongoose.Schema({
     title: {type: String, required: true},
     body: {type: String, required: true},
-    user: {type: String,}
+    user: {type: mongoose.Types.ObjectId}
 })
 
 export const PostModel = mongoose.model<Post>('post', postSchema); 
