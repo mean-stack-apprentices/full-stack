2ae2068bd8afd6050450f7fd5bdc7edba07e6065
mekdes45 <mekmis2020@gmail.com>
2021-09-22 20:54:44 -0700

    feat(server,route):add routes for posts

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,70 +1,110 @@
 import express from 'express';
 import cors from 'cors';
 import path from 'path';
-import fs from 'fs';
+
 import mongoose from 'mongoose';
 import { UserModel } from './schemas/user.schema.js';
+import { PostModel } from './schemas/post.schema.js';
 
-mongoose .connect('mongodb://localhost:27017/test')
-  .then(() => {
-    console.log('connected to db');
-  }).catch(err => console.log(err, 'connecting to db'));
+ mongoose.connect('mongodb://localhost:27017/test').then(() => {
+      console.log('connected to db');
+  }).catch(err => console.log(err, 'connecting to db'))
+
 
   const app = express();
-const __dirname = path.resolve();
-const PORT = 3501;
+  const __dirname = path.resolve();
+  const PORT = 3501;
+  
 
-app.use(cors());
+  app.use(cors());
+  app.use(express.json())
+  
+  app.get('/create-user', function (req, res) {
+    const user = new UserModel({
+      name: 'mekdes',
+      email:'mekdes123@bitwise.com'
+    });
+      user.save().then(user => {
+        console.log(user, 'saved')
+        res.json({ data: user });
+      });
+  });
+       
+  app.get('/create-post', function (req, res) {
+    const post = new PostModel({
+      title: 'first meanstack project',
+      body:'',
+    });
+      post.save().then(post => {
+        console.log(post, 'saved')
+        res.json({ data: post });
+      });
+       });
+      
 app.get('/', function(req, res) {
-  res.json({message:'test'});
+   res.json({message:'test'});
 });
 
 
-app.get('/users', function (req, res) {
-  UserModel.find().then(users => {
-    console.log('found users', users);
-    res.json({data:users})
-});
-
-});
-
-app.get('/create-user', function (req, res) {
-  const user = new UserModel({
-    name: 'Mekdes',
-    email: 'mekdesb123@bitwise.com'
-  
-  });
-
-  // app.get('/create-user', function (req, res) {
-  //   const user = new UserModel({
-  //     name: 'sol',
-  //     email: 'solomon@bitwise.com'
-    
-  //   });
-
-    user.save().then(user => {
-      console.log(user, 'saved')
-      res.json({ data: user });
+app.get('/posts', function (req, res) {
+  PostModel.find()
+    .then((posts) =>
+      res.json({ data: posts }))
+    .catch((err) => {
+      res.status(501);
+      res.json({ errors: err })
     })
+})
+
+app.get('/users', function(req,res){
+    UserModel.find()
+    .then((users) => 
+      res.json({data:users}))
+    .catch((err) => {
+      res.status(404);
+      res.json({error: err})
+    })
+});
+
+app.post('/create-user',function(req,res){
+  const newUser = new UserModel({
+    email: req.body.email,
+    name: req.body.name,
   });
-  app.post('/create-users', function (req, res) {
-    console.log('posted', req.body);
-  
-    const users = JSON.parse(fs.readFileSync(path.join(__dirname, 'mongodb://localhost:27017/test'), 'utf8'));
-    
-    users.push(req.body);
-    fs.writeFileSync(path.join(__dirname, 'users.json'), JSON.stringify(users))
-  
-    
-    res.sendFile(path.join(__dirname, 'users.json'));
-     
+
+  newUser.save()
+  .then(user => {
+    res.json({data: user});
+  })
+  .catch((err) => {
+   res.status(404);
+   res.json({error: err})
+ })
+
+});
+
+
+app.post('/create-post', function (req, res) {
+  const { title, body } = req.body;
+  const post = new PostModel({
+    title,
+    body,
   });
+
+  post.save()
+  .then(post => {
+    res.json({data: post});
+  })
+  .catch((err) => {
+   res.status(404);
+   res.json({error: err})
+ })
+
+});
+
+
 
 
   app.listen(PORT, function () {
     console.log(`starting at localhost http://localhost: ${PORT}`);
   })
-
-
-
-
