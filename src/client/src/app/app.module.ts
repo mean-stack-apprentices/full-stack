625851b1edd096446ca8389bbd6e56d553a0b05b
ChelleAmon <amongichelle@gmail.com>
2021-09-23 09:09:36 -0700

    feat(route): add navigation, router-outlet

--- src/client/src/app/app.module.ts
+++ src/client/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AddUserComponent } from './components/add-user/add-user.component';
 import { FormsModule } from '@angular/forms';
+import { AddPostComponent } from './components/add-post/add-post.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    AddUserComponent
+    AddUserComponent,
+    AddPostComponent,
+    NavigationComponent
   ],
   imports: [
     BrowserModule,
