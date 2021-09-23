19de6ca84cbac41f24bc50b57aabf75dfa2519e0
sirisha v <svoggu@alphaworks.tech@gmail.com>
2021-09-22 21:38:39 -0700

    feat(server,schema): add schemas,post users,posts

--- src/server/server.ts
+++ src/server/server.ts
@@ -3,11 +3,13 @@ import cors from 'cors';
 import fs from 'fs';
 import path from 'path';
 import mongoose from 'mongoose';
-import { UserModel } from './schemas/user.schema';
- 
+import { UserModel } from './schemas/user.schema.js';
+ import { PostModel } from './schemas/post.schema.js';
 const app = express();
 const __dirname = path.resolve();
 const PORT = 3501;
+
+app.use(express.json());
 
 mongoose.connect('mongodb://localhost:27017/test')
 .then(() => {
@@ -37,19 +39,46 @@ app.post('/create-user', function(req,res){
     const user = new UserModel({
         name,
         email,
-    });
-    
-    user.save()
-    .then((data) => {
-        res.json({data});
+    })
+user.save()
+    .then(user => {
+        res.json({data:user});
     })
     .catch(err => {
-        res.status(501)
-        res.json({errors: err})
+        res.status(501);
+        res.json({errors: err});
     })
+    
 });
 
+app.get('/posts', function(req,res){
+        PostModel.find()
+        .then(posts => res.json({data: posts}))
+        .catch(err => {
+            res.status(502)
+            res.json({errors: err})
+        })
+    });
+
+
+app.post('/create-post', function(req,res){
+        const {title,body} = req.body;
+        const post = new PostModel({
+            title,
+            body,
+        })
+
+        post.save()
+        .then(post => {
+            res.json({data:post});
+        })
+        .catch(err => {
+            res.status(501);
+            res.json({errors: err});
+        })
+});
 
 app.listen(PORT, function(){
     console.log( `starting at localhost http://localhost: ${PORT}`);
-})
+}
+)
