6d178716e1044f9d3d7dbc1be839f8ede4f9c8ae
sabiyoung <ethiodatecakes@gmail.com>
2021-09-28 14:16:31 -0700

    feat(user.effect): add user effect

--- src/client/src/app/store/effects/user/user.effects.ts
+++ src/client/src/app/store/effects/user/user.effects.ts
@@ -1,13 +1,28 @@
 import { Injectable } from '@angular/core';
-import { Actions, createEffect } from '@ngrx/effects';
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { catchError, map, mergeMap } from 'rxjs/operators';
+import { loadUsers, loadUsersFailure } from '../../actions/user/user.actions';
+import { loadUsersSuccess } from 'src/app/store/actions/user/user.actions';
+import { of } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
 
 
 
 @Injectable()
 export class UserEffects {
+loadUsers$ = createEffect(() => this.actions$.pipe(
+  ofType(loadUsers),
+  mergeMap( () => this.userService.getUsers()
+  .pipe( map((data) => loadUsersSuccess({data})),
+  catchError(error => {
+    return of(loadUsersFailure((error)))
+  })
+  )
+  )
+)
+)
 
-
-
-  constructor(private actions$: Actions) {}
+  constructor(private actions$: Actions,
+    private userService: UserService) {}
 
 }
