d5e2a6846680fd853ffc4bcbdd24e7b6a83dee4d
sabiyoung <ethiodatecakes@gmail.com>
2021-09-28 14:23:46 -0700

    feat(user-list): imported store

--- src/client/src/app/components/users-list/users-list.component.ts
+++ src/client/src/app/components/users-list/users-list.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
+import { AppState } from 'src/app/store';
+import { loadUsers, selectUserAction } from 'src/app/store/actions/user/user.actions';
+import { usersSelector } from 'src/app/store/selectors/user/user.selectors';
 import { User } from '../../../../../shared/models/user.model';
+import { Store } from '@ngrx/store';
 
 @Component({
   selector: 'app-users-list',
@@ -10,11 +14,15 @@ import { User } from '../../../../../shared/models/user.model';
 })
 export class UsersListComponent implements OnInit {
   users$: Observable<User[]>
-  constructor(private userService: UserService) {
-    this.users$ = this.getUsers()
+  constructor(
+        private userService: UserService,
+        private store: Store<AppState>
+        ) {
+    this.users$ =   this.users$ = this.store.select(usersSelector)
   }
 
   ngOnInit(): void {
+    this.store.dispatch(loadUsers())
   }
 
   getUsers() {
@@ -26,12 +34,10 @@ export class UsersListComponent implements OnInit {
     console.log(`user '${name}' deleted successfully`);
   }
 
-  selectUser(id: any) {
-    this.userService.selectUser(id);
-  } 
+
 
   checkSelected(id: any) {
     return this.userService.selectedUserId == id? 'green' : 'black'
   }
-
+  
 }
