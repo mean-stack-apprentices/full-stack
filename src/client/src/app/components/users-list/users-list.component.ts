fb758d3c60d3523981cf45bffa4b24ae28824162
vinsonf <vfernandez@shift3tech.com>
2021-09-28 10:01:31 -0700

    feat(actions): generate load user actions

--- src/client/src/app/components/users-list/users-list.component.ts
+++ src/client/src/app/components/users-list/users-list.component.ts
@@ -1,6 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { UserService } from 'src/app/services/user.service';
+import { AppState } from 'src/app/store';
+import { loadUsers } from 'src/app/store/actions/user/user.actions';
 import { User } from '../../../../../shared/models/user.model';
 
 @Component({
@@ -10,11 +13,15 @@ import { User } from '../../../../../shared/models/user.model';
 })
 export class UsersListComponent implements OnInit {
   users$: Observable<User[]>
-  constructor(private userService: UserService) {
+  constructor(
+    private userService: UserService,
+    private store: Store<AppState>,
+    ) {
     this.users$ = this.getUsers()
   }
 
   ngOnInit(): void {
+    this.store.dispatch(loadUsers());
   }
 
   getUsers() {
@@ -28,7 +35,7 @@ export class UsersListComponent implements OnInit {
 
   selectUser(id: any) {
     this.userService.selectUser(id);
-  } 
+  }
 
   checkSelected(id: any) {
     return this.userService.selectedUserId == id? 'green' : 'black'
--- src/client/src/app/store/reducers/user/user.reducer.ts
+++ src/client/src/app/store/reducers/user/user.reducer.ts
@@ -1,5 +1,6 @@
 import { Action, createReducer, on } from '@ngrx/store';
 import { User } from '../../../../../../shared/models/user.model';
+import { loadUsers } from '../../actions/user/user.actions';
 
 
 export const userFeatureKey = 'user';
