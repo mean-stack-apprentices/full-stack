cedc5302275389d6bca94609a27b35527fcec165
sabiyoung <ethiodatecakes@gmail.com>
2021-09-28 14:17:09 -0700

    feat(user.reducer): add reducer

--- src/client/src/app/store/reducers/user/user.reducer.ts
+++ src/client/src/app/store/reducers/user/user.reducer.ts
@@ -1,19 +1,27 @@
 import { Action, createReducer, on } from '@ngrx/store';
+import { User } from '../../../../../../shared/models/user.model';
+import { loadUsersSuccess } from '../../actions/user/user.actions';
+
 
 
 export const userFeatureKey = 'user';
 
 export interface State {
-
+  users: User[]
+  selectedUser: User | null
 }
 
 export const initialState: State = {
-
+users: [],
+selectedUser: null
 };
 
 
 export const reducer = createReducer(
   initialState,
+  on(loadUsersSuccess, (state, action) => {
+    return {...state, users: action.data}
+  })   // to update state
+
 
 );
-
