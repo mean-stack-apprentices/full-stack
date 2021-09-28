ab9d08c8681a52b4678fd3d164cf2d5ab3fd7476
sabiyoung <ethiodatecakes@gmail.com>
2021-09-28 09:31:15 -0700

    feat(server) add delete and Update methods

--- src/server/server.ts
+++ src/server/server.ts
@@ -1,81 +1,104 @@
-
-import express from 'express';
-import cors from 'cors';
-import path from 'path';
-import { PostModel } from './schemas/post.schema.js';
-import { UserModel } from './schemas/user.schema.js'
-import mongoose from 'mongoose';
+import express from "express";
+import cors from "cors";
+import path from "path";
+import { PostModel } from "./schemas/post.schema.js";
+import { UserModel } from "./schemas/user.schema.js";
+import mongoose from "mongoose";
 
 const app = express();
 const __dirname = path.resolve();
 const PORT = 3501;
 
-mongoose.connect('mongodb://localhost:27017/test')
-.then(() => {
-    console.log('Connected to DB Successfully');
-})
-.catch(err => console.log('Failed to Connect to DB', err))
-
-
+mongoose
+  .connect("mongodb://localhost:27017/test")
+  .then(() => {
+    console.log("Connected to DB Successfully");
+  })
+  .catch((err) => console.log("Failed to Connect to DB", err));
 
 app.use(cors());
 app.use(express.json());
 
-app.get('/', function(req, res) {
-   res.json({message:'test'});
+app.get("/", function (req, res) {
+  res.json({ message: "test" });
 });
 
-app.get('/posts', function(req,res){
-    PostModel.find()
-    .then(data => res.json({data}))
-    .catch(err => {
-        res.status(501)
-        res.json({errors: err});
-    })
-});
-
-app.get('/users', function(req,res){
-    UserModel.find()
-    .then(users => res.json({data: users}))
-    .catch(err => {
-        res.status(501)
-        res.json({errors: err});
-    })
-});
-app.post('/create-user', function(req,res){
-    const {name, email, username} = req.body;
-    const user = new UserModel({
-        name,
-        username,
-        email,
+app.get("/posts", function (req, res) {
+  PostModel.find()
+    .then((data) => res.json({ data }))
+    .catch((err) => {
+      res.status(501);
+      res.json({ errors: err });
     });
-    user.save()
-    .then((data) => {
-        res.json({data});
-    })
-    .catch(err => {
-        res.status(501);
-        res.json({errors: err});
-    })
 });
 
-app.post('/create-post', function(req,res){
-    const {title, body} = req.body;
-    const post = new PostModel({
-        title,
-        body,
+app.get("/users", function (req, res) {
+  UserModel.find()
+    .then((users) => res.json({ data: users }))
+    .catch((err) => {
+      res.status(501);
+      res.json({ errors: err });
     });
-    post.save()
+});
+app.post("/create-user", function (req, res) {
+  const { name, email, username } = req.body;
+  const user = new UserModel({
+    name,
+    username,
+    email,
+  });
+  user
+    .save()
     .then((data) => {
-        res.json({data});
+      res.json({ data });
     })
-    .catch(err => {
-        res.status(501);
-        res.json({errors: err});
-    })
+    .catch((err) => {
+      res.status(501);
+      res.json({ errors: err });
+    });
 });
 
-
-app.listen(PORT, function(){
-    console.log( `starting at localhost http://localhost:${PORT}`);
-})
+app.post("/create-post", function (req, res) {
+  const { title, body } = req.body;
+  const post = new PostModel({
+    title,
+    body,
+  });
+  post
+    .save()
+    .then((data) => {
+      res.json({ data });
+    })
+    .catch((err) => {
+      res.status(501);
+      res.json({ errors: err });
+    });
+});
+app.delete("/delete-user/:id", function (req, res) {
+  const _id = req.params.id;
+  UserModel.findByIdAndDelete(_id).then((data) => {
+    res.json({ data });
+  });
+});
+app.put("/update-user/:id", function (req, res) {
+  console.log("Update user");
+  UserModel.findByIdAndUpdate(
+    req.params.id,
+    {
+      $set: { name: req.body.name, username: req.body.username },
+    },
+    {
+      new: true,
+    },
+    function (err, updateUser) {
+      if (err) {
+        res.send("Error updating user");
+      } else {
+        res.json(updateUser);
+      }
+    }
+  );
+});
+app.listen(PORT, function () {
+  console.log(`starting at localhost http://localhost:${PORT}`);
+});
